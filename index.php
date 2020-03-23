fb133a6b67e762423d86c707f0c6c500076af936
phaitoon-srinil <62560839+phaitoon-srinil@users.noreply.github.com>
2020-03-24 00:41:01 +0700

    final test

--- index.php
+++ index.php
@@ -4,5 +4,8 @@
 <H1>
 <?php echo "รหัสนิสิต-62310xxx"; ?><br>
 <?php echo "ชื่อ-สกุล นาย........."; ?><br>
-</H1> Your web browser is <?php echo $HTTP_USER_AGENT; ?>. </BODY>
+<iframe width="500" height="500"
+src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
+</iframe>
+</BODY>
 </HTML>
