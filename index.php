f6d9e6aa177767a355129c3c7cbe22043f2dab0b
phaitoon-srinil <62560839+phaitoon-srinil@users.noreply.github.com>
2020-03-24 00:15:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>BUU Midterm Testing</h1>
-
-<?php
-echo "Hello World!";
-?>
-<iframe width="500" height="500"
-src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
-</iframe>
-</body>
-</html>
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310xxx"; ?><br>
+<?php echo "ชื่อ-สกุล นาย........."; ?><br>
+</H1> Your web browser is <?php echo $HTTP_USER_AGENT; ?>. </BODY>
+</HTML>
