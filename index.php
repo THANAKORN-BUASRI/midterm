6fba06ec80f50254a0df691f90892d54d6ed61f7
phaitoon-srinil <62560839+phaitoon-srinil@users.noreply.github.com>
2020-03-23 23:51:43 +0700

    add php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>BUU Midterm Testing</h1>
+
+<?php
+echo "Hello World!";
+?>
+<iframe width="500" height="500"
+src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
+</iframe>
+</body>
+</html>
